4dfa1f23b0312e5dceee8bd55201e5cefb95646b
srl <1244961548@qq.com>
2017-03-20 11:26:01 +0800

    3.20

--- app/scripts/controllers/homepage.js
+++ app/scripts/controllers/homepage.js
@@ -8,6 +8,21 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp')
-  .controller('homepageCtrl', function () {
+  .controller('homepageCtrl', ["$scope",function ($scope) {
+	$scope.num = 0;
+  	$scope.srl = function(){
+  		$scope.num++;
+  		if ($scope.num%2 == 1) {
+  			$(".srl-showtop").css("opacity",1).css("z-index","99");
+//			$scope.sr = true;
+  		} else{
+  			$(".srl-showtop").css("opacity",0).css("z-index","-1");
+//			$scope.sr = false;
+  		}
+  	}
     
-  });
+    $(".a").click(function(){
+    	alert(1)
+    })
+    
+  }]);
