eec042b84967b55fc8a2326e77da2979846b5685
srl <1244961548@qq.com>
2017-03-20 17:04:20 +0800

    3.20 17:04

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,6 +8,50 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp')
-  .controller('loginCtrl', function () {
+  .controller('loginCtrl', function ($scope,$http) {
+  
+  	$scope.username = '';
+	$scope.user = function(){
+		var name = /^[a-zA-Z]/;
+		if($scope.username == ''){
+			alert("请输入用户名");
+		}else {
+			if (name.test($scope.username) == false) {
+				alert("用户名为字母缩写")
+			}
+		}
+	}
+	
+	$scope.password = ''
+	$scope.tel = function(){
+		var reg = /^1[3|4|5|7|8][0-9]{9}$/; //验证规则
+		
+		if($scope.password == ''){
+			alert("请输入密码");
+		}else {
+			if (reg.test($scope.password) == false) {
+				alert("请输入正确的密码")
+			}
+			
+		}
+	}
+	
+  	$scope.submit = function(){
+		$http({
+    	url:"http://"+ip+"/users/login",
+    	method:"POST",
+    	data:{
+    		"username":$scope.username,
+    		"password":$scope.password
+    	}
+    }).then(function(e){
+    	console.log($scope.e);
+    	
+    	
+    })
+
+}
+
     
-  });
+    
+ })
