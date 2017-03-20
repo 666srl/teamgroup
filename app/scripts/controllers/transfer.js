1a1b897a8d99fff98dcb065dc738faa68e187c6d
panjin <1914982057@qq.com>
2017-03-20 20:24:27 +0800

    12

--- app/scripts/controllers/transfer.js
+++ app/scripts/controllers/transfer.js
@@ -8,6 +8,21 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp')
-  .controller('transferCtrl', function () {
-    
-  });
+  .controller('transferCtrl', ["$scope","$http",function ($scope,$http) {
+    $scope.shenqing = function(){
+    	
+    	$http({
+    		url:"http://"+ip+"/transfer",
+    		method:"post",
+    		data:{
+    			diaorubumen:$scope.bumen,
+    			diaorugangwei:$scope.gangwei,
+    			diaorudidian:$scope.didian,
+    			diaoruyuanyin:$scope.btn,
+    			
+    		}
+    	}).then(function(data){
+    		console.log(data);
+    	})
+    }
+  }]);
