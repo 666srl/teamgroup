0e3156671c1b83d03751a430ed55fd122e8047f6
NiuNiu <NiuNiu>
2017-03-21 15:12:28 +0800

    yukeai

--- app/scripts/controllers/applyfor.js
+++ app/scripts/controllers/applyfor.js
@@ -8,7 +8,42 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp')
-  .controller('applyforCtrl', ["$scope",function($scope){
-  	
-  
-  }]);
+  .controller('applyforCtrl',function($scope,$http){
+	$scope.arr = [];
+	$http({
+			url:"http://"+ip+"/users/"+localStorage.loid,
+
+			method:"get",
+			
+		}).then(function(tada){
+			//console.log(tada)
+			$scope.arr = tada.data;
+		})
+	$scope.send = function(){
+		if($scope.coms ==''||$scope.money ==''||$scope.months == ''|| $scope.date == ''){
+				alert("请将信息填写完整")
+			}else{
+				
+		$http({	
+			url:"http://"+ip+"/apply-for",
+			method:"post",
+			data:{
+				xiangmumingcheng:$scope.coms,
+				jine:$scope.money,
+				yuefen:$scope.months,
+				shijian:$scope.date,
+				status:1,
+				uid:localStorage.loid
+			}
+		}).then(function(data){
+			//console.log(data)
+			$scope.coms = '';
+			$scope.money = '';
+			$scope.months = '';
+			$scope.date = '';
+		})
+	
+			}	
+			}
+  });
+		
