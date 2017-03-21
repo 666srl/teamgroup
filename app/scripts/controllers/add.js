c5c67e21a47240f7c2c842ce8cd35f7bf1824a9b
DuJiang <781289505@qq.com>
2017-03-21 08:23:33 +0800

    3.21dj

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -8,20 +8,27 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp')
-  .controller('addCtrl', ["$scope","$http",function($scope,$http){
-  	$scope.name=""
-  	$scope.zhiwei=""
-  	$scope.user=""
-  	$scope.pass=""
-  	$scope.tel=""
+  .controller('addCtrl', function($scope,$http,$state){
   	
-  $scope.x = false;
+  	$scope.f = true;
+  	$scope.arr =[]
+	$http({
+		 		url:"http://"+ip+"/users",
+		 		method:"get",
+		 	}).then(function(data){
+		 		console.log(data)
+		 		$scope.arr =data.data
+		 		
+		 	})
+    $scope.x = false;
   $scope.tj = function(){
   	$http({
-		 		url:"47.88.16.225:404/users"
-		 		,method:"post",
+		 		url:"http://"+ip+"/users",
+		 		method:"POST",
 		 		data:{
 		 				username:$scope.user,
+		 				bumen:$scope.bumen,
+		 				didian:$scope.didian,
 		 				password:$scope.pass,
 		 				tel:$scope.tel,
 		 				zhiwei:$scope.zhiwei,
@@ -29,14 +36,42 @@ angular.module('teamGroupApp')
 		 				
 		 		}
 		 	}).then(function(data){
-		 		console.log(data)
+//		 		console.log(data)
+		 		$scope.name=""
+				$scope.zhiwei=""
+				$scope.user=""
+				$scope.pass=""
+				$scope.tel=""
+				$scope.bumen=""
+	
 		 	})
   }
+  
+  
+  
+  $scope.sc = function(){
+  	$http({
+		 		url:"http://"+ip+"/users",
+		 		method:"get",
+		 	}).then(function(data){
+//		 		console.log(data)
+		 		$scope.arr =data.data
+		 	})
+  }
+  
+  
+  
+  
   $scope.xs = function(){
   	$scope.x = true;
+  	$scope.f = false;
   }
   $scope.qx = function(){
   	$scope.x = false;
+  	$scope.f = true;
+  	
   }
-  
-  }]);
+  $scope.tuichu = function(){
+  	$state.go("homepage")
+  }
+  });
