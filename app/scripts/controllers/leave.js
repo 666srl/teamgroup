7065b769b9a3ccb12b9833838d1ce6d4627a5416
NiuNiu <NiuNiu>
2017-03-22 08:39:40 +0800

    123

--- app/scripts/controllers/leave.js
+++ app/scripts/controllers/leave.js
@@ -8,19 +8,7 @@
  * Controller of the teamGroupApp
  */
 angular.module('teamGroupApp').controller('leaveCtrl',function($scope,$http){
-  	$http({
-  		url:"http://"+ip+"/users/",
-      	method:"post",
-      	data:{
-      	    xingming:$scope.xingming,
-      	    bumen:$scope.bumen,
-      	    gangwei:$scope.gangwei,
-      	    yuanyin:$scope.yuanyin,
-      	    shijian:$scope.shijian
-      	}
-  	}).then(function(e){
-  		console.log(e)
-  	})
+  
   		
   	})
 
