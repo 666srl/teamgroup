7b3a91ded20ca5a5cf2ad4bf0d529349f901a9fc
gtf <1928146162@qq.com>
2017-03-27 14:09:26 +0800

    3.27gtf

--- app/scripts/controllers/vacation.js
+++ app/scripts/controllers/vacation.js
@@ -33,5 +33,46 @@ angular.module('teamGroupApp')
     		$scope.yc=true
     	}
     }
-    
+    $scope.yc_qd=function(){
+    		$http({
+	    		method:"post",
+	    		url:"http://"+ip+"/vacation",
+	    		data:{
+	    			yuanyin:$scope.yy,
+	    			time1:$scope.starts,
+	    			time2:$scope.dates,
+	    			status:1,
+	    			uid:localStorage.loid,
+	    			zhiwei:sessionStorage.zhiwei,
+	    			xingming:sessionStorage.xingming,
+	    			tel:sessionStorage.tel
+	    		}
+	    	})
+	    	.then(function(ev){
+	    		//console.log(ev)
+	    		$scope.yc=false
+	    		$scope.yy=""
+			  	$scope.starts=""
+			  	$scope.dates=""
+			  	$state.go("homepage")
+	    	},function(error){
+	    		alert("请求失败，联系后台技术人员")
+	    	})
+    }
+    $scope.tx_qx=function(){
+    	$state.go("homepage")
+    }
+   	$scope.yc_qx=function(){
+    	$scope.yc=false
+    	$scope.yy=""
+	  	$scope.starts=""
+	  	$scope.dates=""
+    }
+   	$scope.tx_fh=function(){
+   		$state.go("homepage")
+   	}
+   	$scope.kong_qx=function(){
+   		$scope.kong_yc=false
+   		
+   	}
   });
