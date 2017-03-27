fb1bbd2b601cae3c35f6c985bbcf07a116010302
1997jhk <1072361042@qq.com>
2017-03-27 13:57:03 +0800

    3.27jhk

--- app/scripts/controllers/boss.js
+++ app/scripts/controllers/boss.js
@@ -9,15 +9,22 @@
  */
 
 angular.module('teamGroupApp')
-	.controller('bossCtrl', function($scope, $http, $state) {	
-		$scope.go=function(){
+	.controller('bossCtrl', function($scope, $http, $state) {
+		$scope.go = function() {
 			$state.go("homepage")
-		}	
+		}
 		$scope.arr = [];
 		$scope.arr1 = [];
 		$scope.arr2 = [];
 		$scope.arr3 = [];
 		$scope.arr4 = [];
+		$scope.lei1 = "";
+		$scope.lei2 = "";
+		$scope.lei3 = "";
+		$scope.lei4 = "";
+		$scope.lei5 = "";
+		$scope.arr5 = [];
+
 		//		$http({
 		//			url: "http://" + ip + "/apply-for",
 		//			method: "get", 
@@ -43,124 +50,134 @@ angular.module('teamGroupApp')
 			method: "get",
 		}).then(function(e) {
 			$scope.arr = e.data
-		})
-		$http({
-			url: "http://" + ip + "/apply-for",
-			method: "get",
-		}).then(function(e) {
-			$scope.arr1 = e.data
-		})
-		$http({
-			url: "http://" + ip + "/businesstrip",
-			method: "get",
-		}).then(function(e) {
-			$scope.arr2 = e.data
-		})
-		$http({
-			url: "http://" + ip + "/vacation",
-			method: "get",
-		}).then(function(e) {
-			$scope.arr3 = e.data
-		})
-		$http({
-			url: "http://" + ip + "/transfer",
-			method: "get",
-		}).then(function(e) {
-			$scope.arr4 = e.data
-			if($scope.arr4.length <= 0) {
-				$scope.e = false;
+			$scope.lei1 = "请假";
+			console.log($scope.arr)
+			for(var i = 0; i < $scope.arr.length; i++) {
+				console.log(i)
+				if(localStorage.loid == $scope.arr[i].uid && localStorage.zt == $scope.arr[i].status) {
+					$scope.arr[i].status="2"
+				}
 			}
 		})
-		$scope.tongyi = function(a,b) {		
-			$http({
-				url: "http://" + ip + "/leave/"+a,
-				method: "put",
-				data: {
-					status:0
+
+				$http({
+					url: "http://" + ip + "/apply-for",
+					method: "get",
+				}).then(function(e){
+					$scope.arr1 = e.data
+					$scope.lei2 ="报销";
+				})
+				$http({
+					url: "http://" + ip + "/businesstrip",
+					method: "get",
+				}).then(function(e) {
+					$scope.arr2 = e.data
+					$scope.lei3 ="出差";
+				})
+				$http({
+					url: "http://" + ip + "/vacation",
+					method: "get",
+				}).then(function(e) {
+					$scope.arr3 = e.data
+					$scope.lei4 ="调休";
+				})
+				$http({
+					url: "http://" + ip + "/transfer",
+					method: "get",
+				}).then(function(e) {
+					$scope.arr4 = e.data
+		          $scope.lei5 ="职位调动";
+				})
+					$scope.tongyi = function(a,b) {
+					$http({
+						url: "http://" + ip + "/leave/"+a,
+						method: "put",
+						data: {
+							status:0
+						}
+					})
+				}			
+				$scope.butongyi = function(a) {
+					$http({
+						url: "http://" + ip + "/leave/"+a,
+						method: "put",
+						data: {
+							status: 1
+						}
+					})
 				}
-			})
-			$scope.i = b;
-		}
-		$scope.butongyi = function(a,b) {
-			$http({
-				url: "http://" + ip + "/leave/"+a,
-				method: "put",
-				data: {
-					status: 1
+				$scope.tongyi1 = function(a) {
+					
+					
+					$http({
+						url: "http://" + ip + "/apply-for/"+a,
+						method: "put",
+						data: {
+							status:0
+						}
+					})
 				}
-			})
-			$scope.i = b;
-		}
-		$scope.tongyi1 = function(a) {
-			$http({
-				url: "http://" + ip + "/apply-for/"+a,
-				method: "put",
-				data: {
-					status:0
+				$scope.butongyi1 = function(a) {
+					$http({
+						url: "http://" + ip + "/apply-for/"+a,
+						method: "put",
+						data: {
+							status: 1
+						}
+					})
 				}
-			})
-		}
-		$scope.butongyi1 = function(a) {
-			$http({
-				url: "http://" + ip + "/apply-for/"+a,
-				method: "put",
-				data: {
-					status: 1
+				$scope.tongyi2 = function(a) {
+					$http({
+						url: "http://" + ip + "/businesstrip/"+a,
+						method: "put",
+						data: {
+							status:0
+						}
+					})
 				}
-			})
-		}
-		$scope.tongyi2 = function(a) {
-			$http({
-				url: "http://" + ip + "/businesstrip/"+a,
-				method: "put",
-				data: {
-					status:0
+				$scope.butongyi2 = function(a) {
+					$http({
+						url: "http://" + ip + "/businesstrip/"+a,
+						method: "put",
+						data: {
+							status: 1
+						}
+					})
 				}
-			})
-		}
-		$scope.butongyi2 = function(a) {
-			$http({
-				url: "http://" + ip + "/businesstrip/"+a,
-				method: "put",
-				data: {
-					status: 1
+				$scope.tongyi3 = function(a) {
+					$http({
+						url: "http://" + ip + "/vacation/"+a,
+						method: "put",
+						data: {
+							status:0
+						}
+					})
 				}
-			})
-		}
-		$scope.tongyi3 = function(a) {
-			$http({
-				url: "http://" + ip + "/vacation/"+a,
-				method: "put",
-				data: {
-					status:0
+				$scope.butongyi3 = function(a) {
+					$http({
+						url: "http://" + ip + "/vacation/"+a,
+						method: "put",
+						data: {
+							status: 1
+						}
+					})
 				}
-			})
-		}
-		$scope.butongyi3 = function(a) {
-			$http({
-				url: "http://" + ip + "/vacation/"+a,
-				method: "put",
-				data: {
-					status: 1
+				$scope.tongyi4 = function(a) {
+					$http({
+						url: "http://" + ip + "/transfer/"+a,
+						method: "put",
+						data: {
+							status:0
+						}
+					})
 				}
-			})
-		}
-		$scope.tongyi4 = function(a) {
-			$http({
-				url: "http://" + ip + "/transfer/"+a,
-				method: "put",
-				data: {
-					status:0
+				$scope.butongyi4 = function(a) {
+					$http({
+						url: "http://" + ip + "/transfer/"+a,
+						method: "put",
+						data: {
+							status: 1
+						}
+					})
 				}
-			})
-		}
-		$scope.butongyi4 = function(a) {
-			$http({
-				url: "http://" + ip + "/transfer/"+a,
-				method: "put",
-				data: {
-					status: 1
-				}
-			})
-		}
 	});
